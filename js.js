afe53c57a1029eff3452778c91799cf0ec1bbfbc
salma-mindy <shellyaka998@gmail.com>
2021-05-07 21:10:47 +0000

    my first site

---
+++ js.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.home-slider').owlCarousel({
+        items:1,
+        nav:true,
+        dots:false,
+        autoplay:true,
+        autoplayTimeout:8000,
+        loop:true
+    });
+});
